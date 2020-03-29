c0b94762ed5682f006239b746d24a52defe761cc
Zeqe G <zeqe.g@icloud.com>
2020-03-28 17:58:41 -0700

    ui

---
+++ Expedition/Tabs/WebsiteImg.swift
@@ -0,0 +1,15 @@
+//
+//  WebsiteImg.swift
+//  Expedition
+//
+//  Created by Zeqiel Golomb on 3/28/20.
+//  Copyright © 2020 The Morning Company. All rights reserved.
+//
+
+import UIKit
+
+class WebsiteImg: UIImageView {
+
+    
+
+}
