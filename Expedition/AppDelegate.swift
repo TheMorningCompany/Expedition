8f005763cf8a5ce926dba2e4b8810d00ae8328e8
Zeqe G <zeqe.g@icloud.com>
2020-04-20 16:52:35 -0700

    fixed the errror

--- Expedition/AppDelegate.swift
+++ Expedition/AppDelegate.swift
@@ -79,7 +79,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         if (UserDefaults.standard.bool(forKey: "keep_cookies")) {
-            OptionsViewController().removeCookies()
+            HistoryTableViewController().removeCookies()
         }
     }
 
@@ -94,7 +94,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         if (UserDefaults.standard.bool(forKey: "keep_cookies")) {
-            OptionsViewController().removeCookies()
+            HistoryTableViewController().removeCookies()
         }
         PersistenceService.saveContext()
     }
