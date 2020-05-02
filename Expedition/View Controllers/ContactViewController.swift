ae0ffc0f5204220e626f608277ba99ad72499409
Zeqe G <zeqe.g@icloud.com>
2020-05-01 23:24:26 -0700

    updated every app icon to be more modern

---
+++ Expedition/View Controllers/ContactViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ContactViewController.swift
+//  Expedition
+//
+//  Created by Zeqiel Golomb on 5/1/20.
+//  Copyright © 2020 The Morning Company. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func youtubeOpen(_ sender: Any) {
+    }
+    @IBAction func instagramOpen(_ sender: Any) {
+    }
+    @IBAction func githubOpen(_ sender: Any) {
+    }
+    @IBAction func emailOpen(_ sender: Any) {
+    }
+    @IBAction func reviewOpen(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
