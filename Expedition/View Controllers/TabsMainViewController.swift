b6999f2e6fd162ef0eede0faf474f7cb45381eb4
Zeqe G <zeqe.g@icloud.com>
2020-10-30 17:55:54 -0700

    ui

---
+++ Expedition/View Controllers/TabsMainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TabsMainViewController.swift
+//  Expedition
+//
+//  Created by Zeqiel Golomb on 10/30/20.
+//  Copyright © 2020 The Morning Company. All rights reserved.
+//
+
+import UIKit
+
+class TabsMainViewController: UIViewController {
+
+    @IBOutlet weak var newTabBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.newTabBtn.layer.cornerRadius = 20.0
+        self.newTabBtn.layer.cornerCurve = .continuous
+    }
+    
+
+
+}
